867067d5fa8d59121af43526c3bb862ca5ddc6a2
Nathan Stewart-Devereux <n.stewart.devereux@gmail.com>
2019-06-24 18:24:14 +1200

    ✏️ Correct typo.

--- src/components/StaticClock.js
+++ src/components/StaticClock.js
@@ -5,7 +5,7 @@ function StaticClock () {
         return (
             <>
                 <h2>Clock at build time</h2>
-                <h2>It is {process.env.MY_VAR_EN}.</h2>
+                <h2>It is {process.env.MY_ENV_VAR}.</h2>
             </>
         );
 }
