37c3abaa6f80172af450321bc6a2e98e4ee4db85
Nathan Stewart-Devereux <n.stewart.devereux@gmail.com>
2019-06-24 13:41:58 +1200

    ✏️ Correct typo.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import {Link} from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Clock from "../components/clock";
+import Clock from "../components/Clock";
 import StaticClock from "../components/StaticClock";
 
 const IndexPage = () => (
